9e446c2a12ceb7fbb28df3a1d9b1e446af2176fc
MKevin2 <matheus.santos1616@etec.sp.gov.br>
2024-04-19 22:56:03 -0300

    Alterando BottomNavigationBarItem

--- lib/main.dart
+++ lib/main.dart
@@ -13,82 +13,89 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         
         appBar: AppBar( // Barra no topo do app
-        title: const Text('Home'),
-        backgroundColor: const Color.fromARGB(255, 255, 28, 12),
-        foregroundColor: Colors.white,
+          backgroundColor: const Color.fromARGB(255, 212, 16, 2), /// Cor de fundo
         ),
 
-        bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color.fromARGB(255, 255, 28, 12),
-        currentIndex: 0,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.hiking_sharp),
-            label: "Cultura",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.food_bank_rounded),
-            label: "Culinária",
-          ),
-        ],
-      ), 
+        bottomNavigationBar: BottomNavigationBar( // Barra de navegação ao final da página
+          backgroundColor: const Color.fromARGB(255, 212, 16, 2),
+          currentIndex: 1, // Aponta o primeiro item exibido, nesse caso, o item em destaque
+          iconSize: 24, // Tamanho dos icones
+          selectedFontSize: 16, // Fonte selecionada
+          unselectedFontSize: 12, // // Fonte não selecionada
+          selectedItemColor: const Color.fromARGB(255, 255, 255, 255), // Ícone ativo
+          unselectedItemColor: const Color.fromARGB(255, 233, 230, 230), // Ícone desativado
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold), // 
+            items: const [
+              BottomNavigationBarItem( // Botão de navegação da cultura
+                icon: Icon(Icons.account_balance_rounded),
+                label: "Cultura",
+              ),
+
+              BottomNavigationBarItem( // Botão de navegação home
+                icon: Icon(Icons.home_rounded),
+                label: "Home",
+              ),
+
+              BottomNavigationBarItem( // Botão de navegação da culinária
+                icon: Icon(Icons.add_business_rounded),
+                label: "Culinária",
+              ),
+            ],
+        ), 
+      
         
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Image.asset(
-              'assets/img/background.png', // Imagem de fundo
-              fit: BoxFit.cover,
-            ),
+        body: Stack( // Empilha widgets dentro do body como filhos
+          fit: StackFit.expand, // Define a proporção dos filhos
+            children: [
+              Image.asset( 
+                'assets/img/background.png', // Caminho da imagem de fundo
+                fit: BoxFit.cover, //Expande imagem por todo o espaço disponível
+              ),
 
-            Center(
-              child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, //Forma de Alinhamento
-                children: [
-                  const Text(
-                    'SAMPA-LEE',
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center, // Tipo de alinhamento
+                  children: [
+                    const Text(
+                      'SAMPA-LEE',
                       style: TextStyle(
-                      fontSize: 70,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 10, 10, 10),
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold, // Texto em negrito
+                        color: Color.fromARGB(255, 10, 10, 10),
+                      ),
                     ),
-                  ),
                   
-                  const Text(
-                    'A CULTURA QUE VOCÊ PROCURA',
+                    const Text(
+                      'A CULTURA QUE VOCÊ PROCURA',
                       style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 10, 10, 10),
+                        fontSize: 25, // Tamanho da fonte
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 10, 10, 10),
+                      ),
                     ),
-                  ),
 
-                  Image.asset(
-                    'assets/img/logolee.png', // Caminho da Logo
-                    height: 300.0, // Altura da imagem
-                    width: 300.0, // Largura da imagem
-                  ),
+                    Image.asset(
+                      'assets/img/logolee.png', // Caminho da Logo
+                      height: 300.0,
+                      width: 300.0,
+                    ),
 
-                   ElevatedButton(
-                    onPressed: () {
-                      // Ações do botão aqui
+                    ElevatedButton(
+                      onPressed: () {
+                      // Ações do botão
                     },
 
                     child: const Text(
                       'LOCALIZAÇÃO',
                       style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 10, 10, 10),
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 10, 10, 10),
                       ),
                     ),
                   ),
-                ],
-              
+                ], // children
+              // Widgets             
               ),
             ),    
           ],
